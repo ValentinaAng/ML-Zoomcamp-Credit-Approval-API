503780a496eccd96a70a251e57a325b5f9d73865
ValentinaStojanovikj <angelovska13@gmail.com>
2024-12-05 16:36:32 +0100

    chore: update route
     - Added logging info

--- app/routers/predict_route.py
+++ app/routers/predict_route.py
@@ -1,6 +1,6 @@
 import pandas as pd
+import structlog
 from fastapi import APIRouter
-
 from app.schemas.input_schema import InputData
 from app.schemas.output_schema import OutputData
 from app.services.latest_model import load_model
@@ -12,6 +12,11 @@ router = APIRouter()
 
 @router.post("/predict")
 def predict_credit_loan(data: InputData) -> OutputData:
+    # Get logger
+    logger = structlog.getLogger()
+
+    logger.info(f"Received prediction request: {data}")
+
     # Convert the input data into a DataFrame
     data_dict = data.model_dump()
     data = pd.DataFrame([data_dict])  # type: ignore[reportArgumentType]
@@ -29,4 +34,5 @@ def predict_credit_loan(data: InputData) -> OutputData:
     status = map_prediction_to_status(predictions_list)
 
     # Return the prediction result
+    logger.info(f"Prediction result: {predictions_list}, status: {status}")
     return OutputData(prediction=predictions_list, status=status)
